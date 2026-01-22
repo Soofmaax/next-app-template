8d41fc9f0b9df26fb2f4c1aecebf11620df3b415
Soofmax <salwaessaf@gmail.com>
2026-01-22 00:53:06 +0000

    feat: add digital menu categories and items (Just Kids, Planches, Entrées, Salades, burgers, pizzas, desserts, cocktails sans alcool)
    Co-authored-by: Cosine <agent@cosine.sh>

--- src/lib/menu-content.ts
+++ src/lib/menu-content.ts
@@ -12,9 +12,508 @@ export interface DigitalMenuCategory {
 
 /**
  * Carte digitale d'aperçu pour la page /menu.
- * Construite à partir de la carte des cocktails du Just Relax.
+ * Construite à partir de la carte du Just Relax (extraits).
  */
 export const digitalMenuCategories: DigitalMenuCategory[] = [
+  {
+    id: "just-kids",
+    name: "Just Kids",
+    items: [
+      {
+        name: "Menu cheese",
+        description:
+          "Cheeseburger servi avec ses frites maison et 1 boule de glace. Boisson au choix : Evian, Ice Tea ou Coca.",
+        price: "9,90 €",
+      },
+      {
+        name: "Menu chicken",
+        description:
+          "Nuggets (x5) servis avec leurs frites maison et 1 boule de glace. Boisson au choix : Evian, Ice Tea ou Coca.",
+        price: "9,90 €",
+      },
+      {
+        name: "Menu Italiano",
+        description:
+          "Tagliatelles bolognaise servies avec 1 boule de glace. Boisson au choix : Evian, Ice Tea ou Coca.",
+        price: "9,90 €",
+      },
+    ],
+  },
+  {
+    id: "planches",
+    name: "Planches à partager",
+    items: [
+      {
+        name: "Planche de fromage",
+        description: "Assortiment de fromages, idéale à partager à deux.",
+        price: "15,00 €",
+      },
+      {
+        name: "Planche de charcuterie",
+        description: "Sélection de charcuteries à partager à deux.",
+        price: "15,00 €",
+      },
+      {
+        name: "Planche mixte",
+        description: "Mélange de fromages et de charcuteries pour 2 personnes.",
+        price: "17,00 €",
+      },
+    ],
+  },
+  {
+    id: "entrees",
+    name: "Entrées",
+    items: [
+      {
+        name: "Œuf mayonnaise",
+        description: "Classique œuf mayonnaise, simple et gourmand.",
+        price: "7,90 €",
+      },
+      {
+        name: "Duo terre mer",
+        description:
+          "Galette de pommes de terre, saumon fumé et fromage frais.",
+        price: "9,90 €",
+      },
+      {
+        name: "Camembert au four entier",
+        description:
+          "Camembert rôti au four, à partager ou à déguster en solo.",
+        price: "10,90 €",
+      },
+      {
+        name: "Burrata tomates",
+        description:
+          "Burrata, tomates, roquette et assaisonnement balsamique.",
+        price: "10,90 €",
+      },
+    ],
+  },
+  {
+    id: "salades",
+    name: "Salades",
+    items: [
+      {
+        name: "Salade César",
+        description:
+          "Salade, tomates cerises, croûtons, œuf dur, copeaux de parmesan, poulet pané.",
+        price: "13,90 €",
+      },
+      {
+        name: "Salade de chèvre chaud",
+        description:
+          "Salade, tomates cerises, œuf dur, croûtons, toasts de chèvre chaud.",
+        price: "13,90 €",
+      },
+      {
+        name: "Salade océane",
+        description:
+          "Salade, tomates cerises, œuf dur, croûtons, saumon fumé.",
+        price: "14,90 €",
+      },
+    ],
+  },
+  {
+    id: "burgers-gourmet",
+    name: "Just gourmet Burger",
+    items: [
+      {
+        name: "Le Veggie",
+        description:
+          "Pain bao, galette végétarienne, galette de pommes de terre, emmental, salade, tomate, cornichons, sauce burger. Servi avec frites maison.",
+        price: "11,90 €",
+      },
+      {
+        name: "Le Smash",
+        description:
+          "Pain brioché, sauce burger, cheddar, steak, bacon de dinde, œuf, oignons crispy, salade, tomates, cornichons. Servi avec frites maison.",
+        price: "12,90 €",
+      },
+      {
+        name: "Le Milanaise",
+        description:
+          "Pain brioché, sauce moutarde, emmental, escalope de poulet pané, salade, tomate, cornichons. Servi avec frites maison.",
+        price: "12,90 €",
+      },
+      {
+        name: "Bao double steak",
+        description:
+          "Pain bao, sauce poivre, double steak de 100g chacun, jambon de poulet, cheddar, salade, tomate, cornichons. Servi avec frites maison.",
+        price: "13,90 €",
+      },
+    ],
+  },
+  {
+    id: "just-express",
+    name: "Just Express",
+    items: [
+      {
+        name: "Hot dog",
+        description: "Hot dog servi avec frites maison.",
+        price: "7,90 €",
+      },
+      {
+        name: "Omelette",
+        description: "Omelette servie avec sa salade.",
+        price: "7,90 €",
+      },
+      {
+        name: "Frites omelette",
+        description: "Omelette servie avec frites maison et salade.",
+        price: "8,90 €",
+      },
+      {
+        name: "Croque fromage",
+        description: "Croque au fromage servi avec frites et salade.",
+        price: "8,90 €",
+      },
+      {
+        name: "Croque-monsieur",
+        description: "Croque-monsieur servi avec frites et salade.",
+        price: "9,90 €",
+      },
+      {
+        name: "Croque madame",
+        description: "Croque-madame servi avec frites et salade.",
+        price: "10,90 €",
+      },
+      {
+        name: "Croque saumon",
+        description: "Croque au saumon servi avec frites et salade.",
+        price: "11,90 €",
+      },
+      {
+        name: "Petit panier de frites",
+        description: "Panier de frites maison (petit format).",
+        price: "3,00 €",
+      },
+      {
+        name: "Grand panier de frites",
+        description: "Panier de frites maison (grand format).",
+        price: "5,00 €",
+      },
+    ],
+  },
+  {
+    id: "pizzas",
+    name: "Pizzas Just Relax",
+    items: [
+      {
+        name: "Margherita",
+        description:
+          "Sauce tomate, basilic, mozzarella fior di latte.",
+        price: "10,90 €",
+      },
+      {
+        name: "Regina",
+        description:
+          "Sauce tomate, jambon de poulet, mozzarella fior di latte, champignons, olives.",
+        price: "12,90 €",
+      },
+      {
+        name: "Neptune",
+        description:
+          "Sauce tomate, mozzarella fior di latte, thon, champignons, olives.",
+        price: "12,90 €",
+      },
+      {
+        name: "Végétarienne",
+        description:
+          "Sauce tomate, mozzarella fior di latte, champignons, oignons, poivrons, olives.",
+        price: "12,90 €",
+      },
+      {
+        name: "Calzone",
+        description:
+          "Sauce tomate, mozzarella fior di latte, jambon de poulet, œuf, champignons.",
+        price: "13,90 €",
+      },
+      {
+        name: "4 fromages",
+        description:
+          "Sauce tomate, mozzarella fior di latte, gorgonzola, parmesan, chèvre.",
+        price: "13,90 €",
+      },
+      {
+        name: "Orientale",
+        description:
+          "Sauce tomate, mozzarella fior di latte, merguez, poivrons, olives, œuf.",
+        price: "13,90 €",
+      },
+      {
+        name: "Campione",
+        description:
+          "Sauce tomate, mozzarella fior di latte, viande hachée, champignons, œuf.",
+        price: "13,90 €",
+      },
+      {
+        name: "Fermière",
+        description:
+          "Crème fraîche, mozzarella fior di latte, poulet, lardons, oignons.",
+        price: "13,90 €",
+      },
+      {
+        name: "Chèvre-miel",
+        description:
+          "Crème fraîche, mozzarella fior di latte, chèvre, miel.",
+        price: "13,90 €",
+      },
+      {
+        name: "L’Indienne",
+        description:
+          "Crème fraîche, curry, mozzarella fior di latte, escalope de poulet, poivrons, oignons.",
+        price: "13,90 €",
+      },
+      {
+        name: "Saumon",
+        description:
+          "Sauce tomate ou crème fraîche, mozzarella fior di latte, saumon fumé.",
+        price: "14,90 €",
+      },
+      {
+        name: "Burrata",
+        description:
+          "Sauce tomate, mozzarella fior di latte, burrata, roquette, tomates cerises, sauce balsamique, copeaux de parmesan.",
+        price: "15,90 €",
+      },
+    ],
+  },
+  {
+    id: "paninis-crepes",
+    name: "Paninis & crêpes salées",
+    items: [
+      {
+        name: "Panini campagnard",
+        description:
+          "Jambon de poulet, crème fraîche, emmental, mozzarella. Servi avec frites maison.",
+        price: "7,90 €",
+      },
+      {
+        name: "Panini 5 fromages",
+        description:
+          "Gorgonzola, chèvre, emmental, mozzarella, cheddar. Servi avec frites maison.",
+        price: "8,90 €",
+      },
+      {
+        name: "Panini chicken",
+        description:
+          "Escalope de poulet, emmental, mozzarella. Servi avec frites maison.",
+        price: "8,90 €",
+      },
+      {
+        name: "Panini viande hachée",
+        description:
+          "Viande hachée, crème fraîche, emmental, mozzarella, cheddar. Servi avec frites maison.",
+        price: "8,90 €",
+      },
+      {
+        name: "Panini nordique",
+        description:
+          "Saumon, crème fraîche, emmental. Servi avec frites maison.",
+        price: "9,90 €",
+      },
+      {
+        name: "Crêpe classique",
+        description:
+          "Œuf, emmental, mozzarella, jambon de poulet. Servie avec sa salade.",
+        price: "8,90 €",
+      },
+      {
+        name: "Crêpe campionne",
+        description:
+          "Crème fraîche, œuf, emmental, mozzarella, viande hachée, champignons. Servie avec sa salade.",
+        price: "9,90 €",
+      },
+      {
+        name: "Crêpe fermière",
+        description:
+          "Crème fraîche, œuf, emmental, mozzarella, escalope de poulet, champignons. Servie avec sa salade.",
+        price: "9,90 €",
+      },
+      {
+        name: "Crêpe 4 fromages",
+        description:
+          "Gorgonzola, emmental, mozzarella, chèvre. Servie avec sa salade.",
+        price: "9,90 €",
+      },
+      {
+        name: "Crêpe norvégienne",
+        description:
+          "Saumon, crème fraîche, emmental, mozzarella. Servie avec sa salade.",
+        price: "10,90 €",
+      },
+    ],
+  },
+  {
+    id: "casse-croutes",
+    name: "Casse-croûtes",
+    items: [
+      {
+        name: "Steak fromage",
+        description:
+          "Pain et frites maison, sauce burger, steak, cheddar, salade, tomate.",
+        price: "11,00 €",
+      },
+      {
+        name: "Chicken pané",
+        description:
+          "Pain et frites maison, sauce moutarde, poulet pané, emmental fondu, salade, tomate.",
+        price: "11,00 €",
+      },
+    ],
+  },
+  {
+    id: "pates",
+    name: "Nos pâtes",
+    items: [
+      {
+        name: "Pâtes à la bolognaise",
+        description:
+          "Pennes ou tagliatelles (selon le jour), sauce bolognaise.",
+        price: "12,90 €",
+      },
+      {
+        name: "Pâtes à la carbonara",
+        description:
+          "Pennes ou tagliatelles (selon le jour), sauce carbonara.",
+        price: "13,90 €",
+      },
+      {
+        name: "Pâtes forestière",
+        description:
+          "Pennes ou tagliatelles (selon le jour), sauce forestière.",
+        price: "13,90 €",
+      },
+      {
+        name: "Pâtes 4 fromages",
+        description:
+          "Pennes ou tagliatelles (selon le jour), sauce aux 4 fromages.",
+        price: "14,90 €",
+      },
+      {
+        name: "Pâtes au saumon",
+        description:
+          "Pennes ou tagliatelles (selon le jour), saumon fumé.",
+        price: "14,90 €",
+      },
+      {
+        name: "Pâtes au pesto burrata",
+        description:
+          "Pennes ou tagliatelles (selon le jour), pesto, burrata.",
+        price: "16,90 €",
+      },
+    ],
+  },
+  {
+    id: "viandes",
+    name: "Nos viandes",
+    items: [
+      {
+        name: "Escalope gratinée",
+        description:
+          "Escalope gratinée, pâtes à la crème.",
+        price: "16,90 €",
+      },
+      {
+        name: "Escalope forestière",
+        description:
+          "Escalope forestière, riz et sauce forestière.",
+        price: "16,90 €",
+      },
+      {
+        name: "Steak haché à cheval",
+        description:
+          "Steak haché à cheval, frites ou haricots verts.",
+        price: "16,90 €",
+      },
+      {
+        name: "Steak sauce poivre",
+        description:
+          "Steak sauce poivre, frites et salade.",
+        price: "17,90 €",
+      },
+    ],
+  },
+  {
+    id: "desserts",
+    name: "Desserts",
+    items: [
+      {
+        name: "Coupe de glace",
+        description:
+          "Deux boules au choix : pistache, chocolat, vanille, fraise, caramel beurre salé.",
+        price: "5,00 €",
+      },
+      {
+        name: "Crêpe au sucre",
+        description: "Crêpe au sucre servie nature.",
+        price: "5,00 €",
+      },
+      {
+        name: "Brioche perdue",
+        description:
+          "Brioche perdue Nutella ou caramel beurre salé, servie avec chantilly.",
+        price: "6,00 €",
+      },
+      {
+        name: "Crêpe Nutella ou caramel",
+        description:
+          "Crêpe Nutella ou caramel beurre salé, servie avec chantilly.",
+        price: "6,00 €",
+      },
+      {
+        name: "Jardinière (tiramisu maison)",
+        description: "Tiramisu maison.",
+        price: "6,00 €",
+      },
+      {
+        name: "Crème brûlée",
+        description: "Crème brûlée classique.",
+        price: "6,00 €",
+      },
+      {
+        name: "Mousse au chocolat",
+        description: "Mousse au chocolat maison.",
+        price: "6,00 €",
+      },
+      {
+        name: "Fondant chocolat",
+        description:
+          "Fondant chocolat servi avec crème anglaise et chantilly.",
+        price: "7,00 €",
+      },
+      {
+        name: "Salade de fruits",
+        description:
+          "Salade de fruits de saison, en fonction des arrivages.",
+        price: "8,00 €",
+      },
+      {
+        name: "Chocolat liégeois glacé",
+        description:
+          "Boules de glace vanille et chocolat, sauce chocolat et chantilly.",
+        price: "8,00 €",
+      },
+      {
+        name: "Coupe glacée caramel",
+        description:
+          "Glace caramel et vanille, sauce caramel beurre salé et chantilly.",
+        price: "8,00 €",
+      },
+      {
+        name: "Café ou thé gourmand",
+        description:
+          "Café ou thé accompagné d’un trio de mignardises.",
+        price: "8,00 €",
+      },
+      {
+        name: "Banana split",
+        description:
+          "Banane, boules de glace vanille, fraise, chocolat et chantilly.",
+        price: "9,00 €",
+      },
+    ],
+  },
   {
     id: "cocktails-sans-alcool",
     name: "Cocktails sans alcool",
